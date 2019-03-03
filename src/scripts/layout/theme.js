94a77aff7da3b2882434600dee7756f1de909f95
ClassicSax <joshshelton309@gmail.com>
2019-03-03 15:41:49 -0600

    yes

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -8,6 +8,7 @@ import 'lazysizes/plugins/respimg/ls.respimg';
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
+
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
 
